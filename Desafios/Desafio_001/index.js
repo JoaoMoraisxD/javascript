47ab9da04b7f0662c957415286976e91ed1da620
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-06-20 21:22:48 -0300

    desafios js

---
+++ Desafios/Desafio_001/index.js
@@ -0,0 +1,9 @@
+let b1 = window.document.getElementById ('b1')
+let b2 = window.document.getElementById ('b2')
+var b3 = window.document.getElementById ('b3')
+
+b1.addEventListener ('click', clicar)
+
+function clicar() {
+    alert ('Clicou no botao 1')
+}
---
+++ Desafios/Desafio_002/index.js
@@ -0,0 +1,7 @@
+let button = window.document.getElementById('bt')
+
+button.addEventListener ('click', clicar)
+
+function clicar(){
+   window.prompt ('Qual seu nome? ')
+}
