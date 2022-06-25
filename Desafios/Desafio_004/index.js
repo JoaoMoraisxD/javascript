c8224a6ce4ee2199c4a7cffc379fe8b0a24c53c9
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-06-24 23:51:11 -0300

    Update index.js

--- Desafios/Desafio_004/index.js
+++ Desafios/Desafio_004/index.js
@@ -6,5 +6,6 @@ function clicar() {
     let produto = window.prompt ('O que deseja comprar ?')
     let valor = window.prompt (`Qual valor do ${produto} ?`)
     let pagamento = window.prompt (`Qual foi o valor que voce deu para pagar o ${produto} ?`)
-    window.alert (`Você comprou ${produto} que custou ${valor}. /n ` `Pagou com ${pagamento} em dinheiro e vai receber` ` ${valor}` - `${pagamento}` `de troco.`)
+    window.alert (`Você comprou ${produto} que custou ${valor}. Pagou com ${pagamento} em dinheiro e vai receber` + ` ${valor - pagamento}` + `de troco.`)
 }
+
