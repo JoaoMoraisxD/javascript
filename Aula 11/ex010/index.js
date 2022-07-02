557942918f6aaa5fb01a949138eb355a6ec3e5d6
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-07-02 03:51:17 -0300

    ...

--- Aula 11/ex010/index.js
+++ Aula 11/ex010/index.js
@@ -1,18 +1,16 @@
 
 
 function calcular() {
-    let txtv = window.document.querySelector('input#txtvel')
-    let res = window.document.querySelector('div#res')
-   let vel = Number(txtv.value)
-
+    var txtv = window.document.querySelector('input#txtvel')
+    var res = window.document.querySelector('div#res')
+    var vel = Number(txtv.value)
     
-    res.innerHTML = `<p> Sua velocidade atual e de <strong>${vel} km/h</strong> </p>`
+    res.innerHTML = `<p> Sua velocidade atual e de <strong> ${vel} km/h </strong> </p>`
   
     if (vel > 60) {
         res.innerHTML += `<p>Voce esta <strong>MULTADO</strong> por excesso de velocidade </p>`
-        res.innerHTML = `<p>Dirija sempre com sinto de segunrança!</p>`
     }
-    res.innerHTML = `<p>Dirija sempre com sinto de segunrança!</p>`
+    res.innerHTML += `<p>Dirija sempre com sinto de segunrança!</p>`
 }
 
 
