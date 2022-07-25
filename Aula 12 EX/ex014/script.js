f1cc136a2af29cee54bb72535d1a26ebfe379466
Joao Morais <106411312+JoaoMoraisxD@users.noreply.github.com>
2022-07-25 19:04:16 -0300

    ex hora do dia

--- Aula 12 EX/ex014/script.js
+++ Aula 12 EX/ex014/script.js
@@ -1,1 +1,20 @@
-alert ('ola')
+function carregar () {
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('img')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} Horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'img/manha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src ='img/tarde.png'
+        document.body.style.background = '#b9846f'
+    }else {
+        //BOA NOITE!
+        img.src = 'img/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
